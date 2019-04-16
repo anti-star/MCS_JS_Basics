ff6364cb25b24f4a295a4e198b96045903f849e1
Tatyana <antistar@yandex.ru>
2019-04-16 13:12:12 +0300

    test_4_8_1-updated

--- test_4_8_1/js/script.js
+++ test_4_8_1/js/script.js
@@ -1,16 +1,14 @@
 function haveEnough(yourMoney, apple, bread, applePrice, breadPrice) {
-	yourMoney = parseFloat(prompt('Сколько вы взяли денег с собой?'));
-	apple = parseFloat(prompt('Сколько купили яблок?'));
-	bread = parseFloat(prompt('Сколько купили хлеба?'));
-	applePrice = parseFloat(prompt('Сколько стоило одно яблоко?')) * apple;
-	breadPrice = parseFloat(prompt('Сколько стоило один батон хлеба?')) * bread;
-
-
-	if (yourMoney >= (applePrice + breadPrice)) {
+	yourMoney = parseFloat(yourMoney);
+	apple = parseFloat(apple);
+	bread = parseFloat(bread);
+	applePrice = parseFloat(applePrice);
+	breadPrice = parseFloat(breadPrice);
+	if (yourMoney >= (applePrice * apple + breadPrice * bread)) {
 		return('Вам хватает денег на покупки');
 	}else {
 		return('Вам не хватает денег');
 	}
 }
 
-document.write(haveEnough());
+document.write(haveEnough(prompt('Сколько у вас денег?'), prompt('Сколько купили яблок?'), prompt('Сколько купили хлеба?'), prompt('Сколько стоило одно яблоко?'), prompt('Сколько стоило один батон хлеба?')));
