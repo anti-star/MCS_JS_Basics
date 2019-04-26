9283babaf5cd91e22dc6cb05dccc9218b97e627b
Tatyana <antistar@yandex.ru>
2019-04-26 17:02:51 +0300

    test_5_9_1_updated

--- test_5_9_1/js/script.js
+++ test_5_9_1/js/script.js
@@ -1,11 +1,13 @@
 let num = 1;
+let counter = 1;
 
 while (num < 100000) {
 	if (num%6 == 0) {
 		let result = document.createElement('div');
-		result.className = 'result ' + num;
+		result.className = 'result ' + counter;
 		result.innerHTML = num;
 		container.appendChild(result);
+		counter++
 	}
 	num++;
 }
